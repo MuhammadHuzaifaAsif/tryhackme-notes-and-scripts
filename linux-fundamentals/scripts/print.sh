6a197550baa95dcd5477f894720b755d9197aa09
MuhammadHuzaifaAsif <asifhuzaifa165@gmail.com>
2025-08-29 23:57:21 +0500

    User_print.sh

---
+++ linux-fundamentals/scripts/print.sh
@@ -0,0 +1,7 @@
+#As we previously discussed, a large selling point of using OSs such as Ubuntu is how lightweight they can be. This, of course, doesn't come without its disadvantages, where for example, often there is no GUI (Graphical User Interface) or what is also known as a desktop environment that we can use to interact with the machine (unless it has been installed). A large part of interacting with these systems is using the "Terminal".
+#The "Terminal" is purely text-based and is intimidating at first. However, if we break down some of the commands, after some time, you quickly become familiar with using the terminal!
+#Command	Description
+#echo	Output any text that we provide
+ech"Hello World"
+#whoami	Find out what user we're currently logged in as!
+whoami
