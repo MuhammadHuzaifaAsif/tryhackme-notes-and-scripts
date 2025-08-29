653fa0438a91dc3453228caa2ecb970a01e5ccd6
MuhammadHuzaifaAsif <asifhuzaifa165@gmail.com>
2025-08-30 02:58:21 +0500

    Create fileediting.sh

---
+++ linux-fundamentals/scripts/fileediting.sh
@@ -0,0 +1,46 @@
+# Up until now, you’ve been putting text into files with echo and > or >>.
+# That works for short stuff, but if you want to write longer text or edit existing files, you need a text editor inside the terminal.
+
+# There are two popular ones:
+
+# 1. Nano (Beginner-Friendly Editor)
+
+# Very easy to use.
+
+# To open (or create) a file:
+
+# nano filename
+
+
+Example:
+
+nano myfile
+
+
+# Once it opens:
+
+# You can just type text directly.
+
+# Use the arrow keys to move around.
+
+# To save: CTRL + O (think: “O” for Output/write).
+
+# To exit: CTRL + X.
+
+# At the bottom of nano you’ll see shortcuts like ^X (which means CTRL+X).
+
+# 2. VIM (Advanced Editor)
+
+# Very powerful but harder to learn at first.
+
+# Has modes (like insert mode for typing, command mode for editing/saving).
+
+# Developers love it because it has:
+
+# Syntax highlighting (colors for code).
+
+# Tons of customizations.
+
+# Works on nearly every Linux system.
+
+👉 For now, use Nano because it’s simple. Later, if you want to be a pro, you can learn VIM.
