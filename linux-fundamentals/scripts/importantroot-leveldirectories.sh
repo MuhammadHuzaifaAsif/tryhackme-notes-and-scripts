144e06b76177a033fab24de254ca7b9083a6ffe3
MuhammadHuzaifaAsif <asifhuzaifa165@gmail.com>
2025-08-30 02:23:55 +0500

    Create importantroot-leveldirectories.sh

---
+++ linux-fundamentals/scripts/importantroot-leveldirectories.sh
@@ -0,0 +1,45 @@
+# 📂 /etc
+
+# Think of this as the settings folder for your whole computer.
+
+# It has things like who the users are (passwd),
+
+# their secret codes (shadow),
+
+# and who is allowed to run admin commands (sudoers).
+
+# 📂 /var
+
+# This is the busy work folder.
+
+# It stores things that are always changing (variable).
+
+# Like logs of what’s happening, temporary app data, and background info for services.
+
+# 📂 /root
+
+# This is the personal home folder for the boss user (root).
+
+# Normal users live in /home/username,
+
+# but the root admin lives here in /root.
+
+# 📂 /tmp
+
+# This is like the scratch pad folder.
+
+# Anyone can put files here,
+
+# but it gets wiped clean every reboot.
+
+# Hackers often drop their tools here since it’s open for everyone.
+
+# 👉 So in everyday words:
+
+# /etc = Settings folder
+
+# /var = Busy data folder
+
+# /root = Boss’s home folder
+
+/tmp = Scratch pad
